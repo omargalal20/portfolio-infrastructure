a3d210ebac60eb49b3d4545d8d3da48f6acd5407
omargalal20 <oelhanafy12@gmail.com>
2025-07-09 06:45:59 -0700

    feat: removed http and https ports in backend sg as cloudflare is handling this, and added deployment file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -46,18 +46,6 @@ module "backend_security_group" {
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     },
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
   ]
 
   egress_rules = [
